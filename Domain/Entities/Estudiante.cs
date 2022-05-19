6f04598d0565d31720d3ce3b382f575bc9d070ae
JADPA10 <JADPA10@JADPA10>
2022-05-19 09:43:33 -0600

    Add project files.

---
+++ Domain/Entities/Estudiante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Estudiante
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Carnet { get; set; }
+        public int Phone { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+
+    }
+}
