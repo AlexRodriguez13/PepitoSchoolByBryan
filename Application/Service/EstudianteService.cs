58b795bd5217a2abad7f826c549c6a2408317c59
Kevin-Mendoza07 <kevingfx71@gmail.com>
2022-05-19 10:21:41 -0600

    a

--- Application/Service/EstudianteService.cs
+++ Application/Service/EstudianteService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.Interface;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace Application.Service
 {
-    class EstudianteService
+    public class EstudianteService : IEstudianteService<Estudiante>
     {
+        public void Añadir(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Eliminar(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FindByCarnet(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FindByLastName(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FindByName(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
