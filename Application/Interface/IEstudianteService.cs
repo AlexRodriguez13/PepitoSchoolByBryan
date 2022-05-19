023fe38b3ceaee90ca45c02d2b8880dd377dcdbb
Kevin-Mendoza07 <kevingfx71@gmail.com>
2022-05-19 10:17:53 -0600

    a

---
+++ Application/Interface/IEstudianteService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interface
+{
+   public interface IEstudianteService<T>
+    {
+        void Añadir(T t);
+        bool eliminar(T t);
+        void FindByName(T t);
+        void FindByLastName(T t);
+        void FindByCarnet(T t);
+        void Update(T t);
+
+    }
+}
