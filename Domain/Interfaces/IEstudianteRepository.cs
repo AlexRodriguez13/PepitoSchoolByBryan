2802644fea151ef4abb9d6a927435ad266f453ed
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-19 21:14:38 -0600

    domain interfaces

---
+++ Domain/Interfaces/IEstudianteRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IEstudianteRepository: IRepository<Estudiante>
+    {
+        Estudiante FindByCarnet(string carnet);
+        List<Estudiante> FindByNames(string names);
+        List<Estudiante> FindByLastnames(string lastnames);
+    }
+}
