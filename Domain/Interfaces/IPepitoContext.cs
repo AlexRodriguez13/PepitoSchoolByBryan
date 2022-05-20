2dd49e002715558c703a0e432bc6a0b905fa5826
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-19 22:34:34 -0600

    a

--- Domain/Interfaces/IPepitoContext.cs
+++ Domain/Interfaces/IPepitoContext.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Domain.Interfaces
---
+++ PepitoschoolApp/Services/EstudianteServices.cs
@@ -0,0 +1,53 @@
+﻿using Domain.Entities;
+using PepitoschoolApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepitoschoolApp.Services
+{
+    public class EstudianteServices : IEstudianteServices
+    {
+        public IEstudianteRepository estudianteRepository;
+        public EstudianteServices(IEstudianteRepository estudianteRepository)
+        {
+            this.estudianteRepository = estudianteRepository;
+        }
+        public void Create(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Estudiante FindByCarnet(string carnet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Estudiante> FindByLastnames(string lastnames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Estudiante> FindByNames(string names)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Estudiante> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(Estudiante t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
