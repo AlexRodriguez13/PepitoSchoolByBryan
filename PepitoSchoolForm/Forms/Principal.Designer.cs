24be2f821d23ccde6f2ebd0b515a6233aadf7d47
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-19 23:32:05 -0600

    a

--- PepitoSchoolForm/Forms/Principal.Designer.cs
+++ PepitoSchoolForm/Forms/Principal.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opcionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarEstudianteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buscarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porNombreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porCarnetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porApellidoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.promedioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(392, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opcionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opcionesToolStripMenuItem
+            // 
+            this.opcionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarEstudianteToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.buscarToolStripMenuItem,
+            this.eliminarToolStripMenuItem,
+            this.promedioToolStripMenuItem,
+            this.modificarToolStripMenuItem});
+            this.opcionesToolStripMenuItem.Name = "opcionesToolStripMenuItem";
+            this.opcionesToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.opcionesToolStripMenuItem.Text = "Opciones";
+            // 
+            // agregarEstudianteToolStripMenuItem
+            // 
+            this.agregarEstudianteToolStripMenuItem.Name = "agregarEstudianteToolStripMenuItem";
+            this.agregarEstudianteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.agregarEstudianteToolStripMenuItem.Text = "Agregar Estudiante";
+            this.agregarEstudianteToolStripMenuItem.Click += new System.EventHandler(this.agregarEstudianteToolStripMenuItem_Click);
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ayudaToolStripMenuItem.Text = "Ayuda";
+            this.ayudaToolStripMenuItem.Click += new System.EventHandler(this.ayudaToolStripMenuItem_Click);
+            // 
+            // buscarToolStripMenuItem
+            // 
+            this.buscarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.porNombreToolStripMenuItem,
+            this.porCarnetToolStripMenuItem,
+            this.porApellidoToolStripMenuItem});
+            this.buscarToolStripMenuItem.Name = "buscarToolStripMenuItem";
+            this.buscarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.buscarToolStripMenuItem.Text = "Buscar";
+            // 
+            // porNombreToolStripMenuItem
+            // 
+            this.porNombreToolStripMenuItem.Name = "porNombreToolStripMenuItem";
+            this.porNombreToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.porNombreToolStripMenuItem.Text = "Por Nombre";
+            // 
+            // porCarnetToolStripMenuItem
+            // 
+            this.porCarnetToolStripMenuItem.Name = "porCarnetToolStripMenuItem";
+            this.porCarnetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.porCarnetToolStripMenuItem.Text = "Por Carnet";
+            // 
+            // porApellidoToolStripMenuItem
+            // 
+            this.porApellidoToolStripMenuItem.Name = "porApellidoToolStripMenuItem";
+            this.porApellidoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.porApellidoToolStripMenuItem.Text = "Por Apellido";
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            this.eliminarToolStripMenuItem.Click += new System.EventHandler(this.eliminarToolStripMenuItem_Click);
+            // 
+            // promedioToolStripMenuItem
+            // 
+            this.promedioToolStripMenuItem.Name = "promedioToolStripMenuItem";
+            this.promedioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.promedioToolStripMenuItem.Text = "Promedio";
+            this.promedioToolStripMenuItem.Click += new System.EventHandler(this.promedioToolStripMenuItem_Click);
+            // 
+            // modificarToolStripMenuItem
+            // 
+            this.modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
+            this.modificarToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.modificarToolStripMenuItem.Text = "Modificar";
+            this.modificarToolStripMenuItem.Click += new System.EventHandler(this.modificarToolStripMenuItem_Click);
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Principal";
             this.Text = "Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opcionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarEstudianteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buscarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem porNombreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem porCarnetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem porApellidoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem promedioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarToolStripMenuItem;
     }
 }
--- PepitoSchoolForm/Forms/Principal.cs
+++ PepitoSchoolForm/Forms/Principal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.Entities;
+using PepitoschoolApp.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,78 @@ namespace PepitoSchoolForm.Forms
 {
     public partial class Principal : Form
     {
+        IEstudianteServices estudiantesServices;
+        int seleccion;
+        public Principal(IEstudianteServices estudiantesServices)
+        {
+            this.estudiantesServices = estudiantesServices;
+            InitializeComponent();
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = estudiantesServices.GetAll();
+            dataGridView1.ClearSelection();
+        }
         public Principal()
         {
             InitializeComponent();
+        }
+
+        private void agregarEstudianteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmEstudiante frmEstudiante = new FrmEstudiante(-50);
+            frmEstudiante.estudiantesServices = estudiantesServices;
+            frmEstudiante.ShowDialog();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = estudiantesServices.GetAll();
+        }
+
+        private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            if (index >= 0)
+            {
+                Estudiante a = estudiantesServices.GetAll()[index];
+                estudiantesServices.Delete(a);
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = estudiantesServices.GetAll();
+            }
+        }
+
+        private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Seleccione toda la fila del estudiante con la flechita de la izquierda (parece boton de play) y de click derecho para ver más opciones");
+        }
+
+        private void promedioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if (index >= 0)
+            {
+                Estudiante a = estudiantesServices.GetAll()[index];
+                int suma = a.Matematica + a.Estadistica + a.Programacion + a.Contabilidad;
+                int promedio = suma / 4;
+                MessageBox.Show($"El promedio de notas del alumno {a.Nombres} {a.Apellidos} es de: {promedio} ");
+            }
+        }
+        private void dataGridView1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void modificarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            if (index >= 0)
+            {
+                FrmEstudiante frmEstudiante = new FrmEstudiante(index);
+                frmEstudiante.estudiantesServices = estudiantesServices;
+                frmEstudiante.ShowDialog();
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = estudiantesServices.GetAll();
+            }
         }
     }
 }
