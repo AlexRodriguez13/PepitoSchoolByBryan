3ca723d27c7266136a7e6ffde578ade1ada8bad2
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-19 22:28:02 -0600

    a

---
+++ PepitoschoolApp/Interfaces/IEstudianteServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepitoschoolApp.Interfaces
+{
+    public interface IEstudianteServices : IService<Estudiante>
+    {
+        Estudiante FindByCarnet(string carnet);
+        List<Estudiante> FindByNames(string names);
+        List<Estudiante> FindByLastnames(string lastnames);
+    }
+}
