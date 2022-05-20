72436dc8e95ee97591fe84ab858037df0342b1cb
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-05-19 21:04:58 -0600

    domain entities

---
+++ Applications/Services/EstudianteService.cs
@@ -0,0 +1,14 @@
+﻿using Applications.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applications.Services
+{
+    public class EstudianteService: IEstudianteService
+    {
+
+    }
+}
